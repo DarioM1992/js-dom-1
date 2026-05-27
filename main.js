d1c6c0ca4f7cd4a31cdcd808395adad084b9f820
DarioM1992 <dario21992@gmail.com>
2026-05-27 18:19:36 +0200

    creato evento per il testo del bottone accendi e spegni

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 const btn = document.querySelector('button');
+const img = document.querySelector('img');
+const imgOff = "img/white_lamp.png";
+const imgOn = "img/yellow_lamp.png";
+
+btn.addEventListener('click' , () => {
+   if (btn.textContent === "\"Accendi\"") {
+    btn.textContent = "\"Spegni\"";
+   } else {
+    btn.textContent = "\"Accendi\"";
+   }
+});
 
